f1c877b4884a2633017758066bc10467c9d778c9
Ertval Karameta <grafido@metagalerie.com>
2026-06-19 18:07:49 +0300

    Make hello-devops.sh dynamically detect username

--- hello-devops.sh
+++ hello-devops.sh
@@ -1,2 +1,21 @@
-#!/bin/bash
-echo "Hello ekaramet!"
+#!/usr/bin/env bash
+
+# Try to get username via gh cli
+username=$(gh api user -q .login 2>/dev/null)
+
+# If gh cli is not logged in or fails, try to parse from git remote origin
+if [ -z "$username" ]; then
+    remote_url=$(git config --get remote.origin.url 2>/dev/null)
+    if [[ "$remote_url" =~ /git/([^/]+)/ ]]; then
+        username="${BASH_REMATCH[1]}"
+    elif [[ "$remote_url" =~ /([^/]+)/piscine-scripting ]]; then
+        username="${BASH_REMATCH[1]}"
+    fi
+fi
+
+# Fallback default if all else fails
+if [ -z "$username" ]; then
+    username="ekaramet"
+fi
+
+echo "Hello ${username}!"
