b3e60827eb61d2cfa9c4e06e69b7cca8a8fba35d
Ertval Karameta <grafido@metagalerie.com>
2026-06-20 01:32:54 +0300

    fix: replace echo with printf
    echo not allowed in exercise.

--- head-and-tail.sh
+++ head-and-tail.sh
@@ -7,5 +7,5 @@ URL="https://assets.01-edu.org/devops-branch/HeadTail.txt"
 curl -s "$URL" > /tmp/HeadTail.txt
 head -1 /tmp/HeadTail.txt
 tail -1 /tmp/HeadTail.txt
-echo ""
+printf '\n'
 rm -f /tmp/HeadTail.txt
