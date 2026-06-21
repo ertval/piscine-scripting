6dd0687874fc0d4cfce640fcf740b3ffd9f4bfad
Ertval Karameta <grafido@metagalerie.com>
2026-06-21 22:46:42 +0300

    fix(division): remove trailing periods from error messages

--- division.sh
+++ division.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 if [ $# -ne 2 ]; then
-	echo "Error: two numbers must be provided."
+	echo "Error: two numbers must be provided"
 elif ! [[ $1 =~ ^-?[0-9]+$ ]] || ! [[ $2 =~ ^-?[0-9]+$ ]]; then
-	echo "Error: both arguments must be integers."
+	echo "Error: both arguments must be integers"
 elif [ $(echo "$2 == 0" | bc) -eq 1 ]; then
-	echo "Error: division by zero is not allowed."
+	echo "Error: division by zero is not allowed"
 else
 	result=$(echo "$1 / $2" | bc)
 	echo "$result"
